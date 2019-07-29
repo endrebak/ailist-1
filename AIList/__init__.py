3643006474228c4dbe73ea3a8c39e9ee91f92193
kylessmith <kyle.smith@stjude.org>
2019-07-29 14:37:39 -0500

    Add .get_include()

--- AIList/__init__.py
+++ AIList/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 from .AIList import AIList
+from .AIList import get_include
 
 # This is extracted automatically by the top-level setup.py.
 __version__ = '0.1.0'
