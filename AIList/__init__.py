c87e3ce7e72a2d70ebfe46d5a09937042719d3ef
kylessmith <kyle.smith@stjude.org>
2019-07-29 15:37:33 -0500

    Add access to Interval

--- AIList/__init__.py
+++ AIList/__init__.py
@@ -1,7 +1,7 @@
 """Init for AIList."""
 
 from __future__ import absolute_import
-from .AIList import AIList
+from .AIList import AIList, Interval
 from .AIList import get_include
 
 # This is extracted automatically by the top-level setup.py.
